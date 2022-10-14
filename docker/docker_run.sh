11ed7bc81bdfa689952cfd6f243e3819106f334f
Zhennong Chen <chenzhennong@gmail.com>
2022-10-14 14:56:31 -0500

    change docker

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -1,11 +1,8 @@
 #/bin/bash
 # 
 # change the following variables to set up code, input and output dirs
-#HOST_WORK_DIR="/home/local/PARTNERS/dw640/docker_example"  # change this to your code directory
-#HOST_DATA_DIR="/home/local/PARTNERS/dw640/mnt/women_health_internal/DBT"  # change this to your data directory
-HOST_WORK_DIR="/home/local/PARTNERS/zc13/Documents"
-# HOST_DATA_DIR="/home/local/PARTNERS/zc13/data"
-HOST_DATA_DIR="/home/local/PARTNERS/dw640/mnt/women_health_internal/DBT"
+HOST_WORK_DIR="/home/local/zc13/Documents"
+HOST_DATA_DIR="/home/mnt/data"
 
 # get host user id to populate to the container
 HOST_USER_ID="$(id -u)"
